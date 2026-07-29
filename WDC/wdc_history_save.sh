c4c506faa21c217e841399ac9dc8117a3606068d
unknown <devg1120@gmail.com>
2026-07-29 11:09:58 +0900

    first commit

---
+++ WDC/wdc_history_save.sh
@@ -0,0 +1,26 @@
+
+
+SCRIPT_DIR="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" && pwd)"
+FILE=$SCRIPT_DIR/_wdc.history
+
+if [ ! -f ${FILE} ]; then
+   touch ${FILE}
+fi
+
+IFS=$'\n'
+for line in `cat ${FILE}`
+do
+#echo ">>" $line
+if [ "$line" = "$PWD" ]; then
+    exit 0
+fi
+done
+
+
+echo $PWD ">>" ${FILE}
+echo $PWD >> ${FILE}
+
+tail -n 10 ${FILE}  > ${FILE}.tmp && mv ${FILE}.tmp ${FILE}
+
+
+
