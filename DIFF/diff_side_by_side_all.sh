e320e7376aa333d611a16cc84a1e1f797a5e7acf
unknown <devg1120@gmail.com>
2026-07-30 21:54:05 +0900

    up

---
+++ DIFF/diff_side_by_side_all.sh
@@ -0,0 +1,4 @@
+
+DIFF_SCRIPT_DIR="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" && pwd)"
+$DIFF_SCRIPT_DIR/_DIFF.sh 2 $1 $2
+
---
+++ DIFF/stk_push.sh
@@ -0,0 +1,25 @@
+
+
+DIFF_SCRIPT_DIR="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" && pwd)"
+FILE=$DIFF_SCRIPT_DIR/_stack
+
+if [ ! -f ${FILE} ]; then
+   touch ${FILE}
+fi
+
+#IFS=$'\n'
+#for line in `cat ${FILE}`
+#do
+#if [ "$line" = "$PWD" ]; then
+#    exit 0
+#fi
+#done
+
+
+echo $PWD ">>" ${FILE}
+echo $PWD >> ${FILE}
+
+tail -n 2 ${FILE}  > ${FILE}.tmp && mv ${FILE}.tmp ${FILE}
+
+
+
