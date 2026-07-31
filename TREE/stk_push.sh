7d22871a15c6ec29c3fe16c8eae8189b70f36b66
devg1120 <devg1120@gmail.com>
2026-08-01 07:34:37 +0900

    up

---
+++ TREE/stk_push.sh
@@ -0,0 +1,33 @@
+
+
+TREE_SCRIPT_DIR="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" && pwd)"
+FILE=$TREE_SCRIPT_DIR/_stack
+
+if [ ! -f ${FILE} ]; then
+   touch ${FILE}
+fi
+
+#IFS=$'\n'
+#for line in `cat ${FILE}`
+#do
+#if [ "$line" = "$PWD" ]; then
+#    exit 0
+#fi
+#done
+
+
+#echo $PWD ">>" ${FILE}
+#echo $PWD >> ${FILE}
+
+if [ -n "$1" ]; then
+   echo $PWD/$1 ">>" ${FILE}
+   echo $PWD/$1 >> ${FILE}
+else
+   echo $PWD ">>" ${FILE}
+   echo $PWD >> ${FILE}
+fi
+
+tail -n 10 ${FILE}  > ${FILE}.tmp && mv ${FILE}.tmp ${FILE}
+
+
+
--- TREE/tree3.sh
+++ TREE/tree3.sh
@@ -1,10 +1,20 @@
 #!/usr/bin/env bash
 
 dir_count=0
+skip_dir_count=0
 file_count=0
 
+EXCLUDE=("node_modules" "dist" "build")
+
+echo "exclude dir"
+for str in ${EXCLUDE[@]}; do
+    echo " $str"
+done
+echo
+#echo TREE
+
 traverse() {
-  dir_count=$((dir_count + 1))
+  #dir_count=$((dir_count + 1))
   local directory=$1
   local prefix=$2
 
@@ -22,18 +32,39 @@ traverse() {
        child_prefix="    "
      fi
 
-     echo "${prefix}${pointer}$child"
-     [ -d "$directory/$child" ] &&
-	 traverse "$directory/$child" "$prefix$child_prefix" ||
-	 file_count=$((file_count + 1))
+
+     if [ -d "$directory/$child" ]; then
+         skip=0
+         for str in ${EXCLUDE[@]}; do
+           #echo $str "  " $child
+           if [ "$str" == "$child" ]; then
+             #echo "skip:" $child
+             skip=1
+             break
+           fi
+         done
+
+	 if [ "$skip" -eq 0 ]; then
+           echo "${prefix}${pointer}$child/"
+           dir_count=$((dir_count + 1))
+	   traverse "$directory/$child" "$prefix$child_prefix" 
+         else
+           echo "${prefix}${pointer}$child/+"
+           skip_dir_count=$((skip_dir_count + 1))
+	 fi
+     else
+           echo "${prefix}${pointer}$child"
+	   file_count=$((file_count + 1))
+     fi
     done
 }
 
-#root="."
-root=$1
+root="."
 [ "$#" -ne 0 ] && root="$1"
 echo $root
 
+#echo  "│ "
 traverse $root ""
 echo
-echo "$((dir_count - 1)) directories, $file_count files"
+echo "$((dir_count )) directories, $file_count files"
+echo "$((skip_dir_count )) skip directories "
