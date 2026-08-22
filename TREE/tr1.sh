680b31b01991d81a7e9b2314b22d00cc39531d84
devg1120 <devg1120@gmail.com>
2026-08-22 11:15:42 +0900

    up

--- TREE/tr1.sh
+++ TREE/tr1.sh
@@ -1,4 +1,6 @@
+if [ "$#" -eq 1 ]; then
 cd $1
+fi
 #pwd;find . | sort | sed '1d;s/^\.//;s/\/\([^/]*\)$/|--\1/;s/\/[^/|]*/|  /g'
 pwd;find .  -not \( -path '*/.git*' -o -path '*/node_modules*' \) | sort | sed '1d;s/^\.//;s/\/\([^/]*\)$/|--\1/;s/\/[^/|]*/|  /g'
 
