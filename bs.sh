d64674b0bbfdac94a12e5ca8c4c5b04ed72cc6ae
devg1120 <devg1120@gmail.com>
2026-08-23 10:40:08 +0900

    up

--- bs.sh
+++ bs.sh
@@ -6,15 +6,15 @@ BSTOOLS_DIR="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" && pwd)"
 source $BSTOOLS_DIR/_bstools_rc
 
 usage() {
-         echo "SESSION     bs s  _s"
-         echo "WORK DIR    bs w  _w"
-         echo "TREE        bs t  _t"
-         echo "DIFF        bs d  _d"
-         echo "FIND        bs f  _f"
-         echo "GREP        bs g  _g"
-         echo "REPLACE     bs r  _r"
-         echo "all         bs *  _*"
-         echo "alias       bs a  _a"
+         echo "SESSION     bs s[_]  _s[_] "
+         echo "WORK DIR    bs w[_]  _w[_] "
+         echo "TREE        bs t[_]  _t[_] "
+         echo "DIFF        bs d[_]  _d[_] "
+         echo "FIND        bs f[_]  _f[_] "
+         echo "GREP        bs g[_]  _g[_] "
+         echo "REPLACE     bs r[_]  _r[_] "
+         echo "all         bs +[_]  _+[_] "
+         echo "alias       bs a  _a       "
 }
 
 if [ "$#" -eq 0 ]; then
@@ -24,37 +24,51 @@ fi
 
 session_() {
           cat ${BSTOOLS_DIR}/SESSION/wdc_help;\
+          if [ -n "$1" ]; then
           cat ${BSTOOLS_DIR}/SESSION/wdc_alias | grep  --color=never alias;\
+          fi
 }
 
 dir_() {
           cat ${BSTOOLS_DIR}/DIR/stk_help;\
+          if [ -n "$1" ]; then
           cat ${BSTOOLS_DIR}/DIR/stk_alias | grep  --color=never alias;\
+          fi
 }
 
 tree_() {
           cat ${BSTOOLS_DIR}/TREE/tree_help;\
+          if [ -n "$1" ]; then
           cat ${BSTOOLS_DIR}/TREE/tree_alias | grep  --color=never alias;\
+          fi
 }
 
 diff_() {
           cat ${BSTOOLS_DIR}/DIFF/diff_help;\
+          if [ -n "$1" ]; then
           cat ${BSTOOLS_DIR}/DIFF/diff_alias | grep  --color=never alias;\
+          fi
 }
 
 find_() {
           cat ${BSTOOLS_DIR}/FIND/find_help;\
+          if [ -n "$1" ]; then
           cat ${BSTOOLS_DIR}/FIND/find_alias | grep  --color=never alias;\
+          fi
 }
 
 grep_() {
           cat ${BSTOOLS_DIR}/GREP/grep_help;\
+          if [ -n "$1" ]; then
           cat ${BSTOOLS_DIR}/GREP/grep_alias | grep  --color=never alias;\
+          fi
 }
 
 replace_() {
           cat ${BSTOOLS_DIR}/REPLACE/replace_help;\
+          if [ -n "$1" ]; then
           cat ${BSTOOLS_DIR}/REPLACE/replace_alias | grep  --color=never alias;\
+          fi
 }
 
 alias_() {
@@ -63,25 +77,46 @@ alias_() {
 
 case ${1} in
   "s" )
-	  session_
+	  session_ 
+     ;;
+  "s_" )
+	  session_ alias
      ;;
   "w" )
-	  dir_
+	  dir_ 
+     ;;
+  "w_" )
+	  dir_ alias
      ;;
   "t" )
-	  tree_
+	  tree_ 
+     ;;
+  "t_" )
+	  tree_ alias
      ;;
   "d")
-	  diff_
+	  diff_ 
+     ;;
+  "d_")
+	  diff_ alias
      ;;
   "f" )
-	  find_
+	  find_ 
+     ;;
+  "f_" )
+	  find_ alias
      ;;
   "g" )
-	  grep_
+	  grep_ 
+     ;;
+  "g_" )
+	  grep_ alias
      ;;
   "r" )
-	  replace_
+	  replace_ 
+     ;;
+  "r_" )
+	  replace_ alias
      ;;
   "+")
 	  (
@@ -92,6 +127,17 @@ case ${1} in
 	  find_
 	  grep_
 	  replace_
+  ) | less
+     ;;
+  "+_")
+	  (
+	  session_ alias
+	  dir_ alias
+	  tree_ alias
+	  diff_ alias
+	  find_ alias
+	  grep_ alias
+	  replace_ alias
   ) | less
      ;;
   "a" )
